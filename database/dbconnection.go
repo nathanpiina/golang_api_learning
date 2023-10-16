4ba394492e0ae85f4dd3c36be8b71e2f1d166388
Nathan Pina <nathanpinadias@gmail.com>
2023-10-16 17:37:48 -0300

    feat: database connection

--- database/dbconnection.go
+++ database/dbconnection.go
@@ -1,1 +1,32 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+func DatabaseConnection() *sql.DB {
+	host := "localhost"
+	port := 5432
+	user := "usuario_banco"
+	password := "senha_usuario"
+	dbname := "nome_banco"
+
+	connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	Db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		panic(err)
+	}
+
+	err = Db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Conexão com o banco de dados PostgreSQL estabelecida com sucesso!")
+
+	return Db
+}
