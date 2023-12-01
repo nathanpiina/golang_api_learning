5818cc3274613d28dd749afe02ea7b885dd948b0
nathanpiina <nathanpinadias@gmail.com>
2023-12-01 11:01:18 -0300

    feat: adjust main file

--- main.go
+++ main.go
@@ -1,47 +1,10 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/nathanpiina/golang_api_learning/person"
+	"github.com/nathanpiina/golang_api_learning/api"
 )
 
 func main() {
-	app := fiber.New()
 
-	app.Post("/people", func(c *fiber.Ctx) error {
-		nickname := c.FormValue("nickname")
-		name := c.FormValue("name")
-		birth := c.FormValue("birth")
-		stack := c.FormValue("stack")
-
-		err := person.AddPeople(nickname, name, birth, stack)
-		if err != nil {
-			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
-		}
-
-		return c.SendString("Usuário adicionado com sucesso")
-	})
-
-	app.Get("/people/:nickname", func(c *fiber.Ctx) error {
-		nickname := c.Params("nickname")
-		result, err := person.SearchPeople(nickname)
-		if err != nil {
-			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
-		}
-
-		return c.SendString(result)
-	})
-
-	app.Get("/count", func(c *fiber.Ctx) error {
-		result, err := person.CountRowsInTable()
-		if err != nil {
-			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
-		}
-
-		return c.SendString(result)
-	})
-
-	person.Main()
-
-	app.Listen(":3000")
+	api.Main()
 }
