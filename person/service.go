66b5a1cfdd98b8f30b634db8cee72f80d87747a5
nathanpiina <nathanpinadias@gmail.com>
2023-12-01 11:00:55 -0300

    feat: insert errors messages

--- person/service.go
+++ person/service.go
@@ -16,43 +16,43 @@ func Main() {
 }
 
 func AddPeople(nickname string, name string, birth string, stack string) error {
-	p := People{Nickname: nickname, Name: name, Birth: birth, Stack: stack}
+    p := People{Nickname: nickname, Name: name, Birth: birth, Stack: stack}
 
-	_, err := Db.Exec("INSERT INTO people (nickname, name, stack, birth) VALUES ($1, $2, $3, $4)", p.Nickname, p.Name, p.Stack, p.Birth)
+    _, err := Db.Exec("INSERT INTO people (nickname, name, stack, birth) VALUES ($1, $2, $3, $4)", p.Nickname, p.Name, p.Stack, p.Birth)
 
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
+    if err != nil {
+        log.Printf("erro ao adicionar pessoa: %v", err)
+        return fmt.Errorf("erro ao adicionar pessoa: %v", err)
+    }
 
-	return nil
+    return nil
 }
 
 func SearchPeople(nickname string) (string, error) {
-	row := Db.QueryRow("SELECT name, birth, stack FROM people WHERE nickname = ($1)", nickname)
+    row := Db.QueryRow("SELECT name, birth, stack FROM people WHERE nickname = ($1)", nickname)
 
-	var name string
-	var birth string
-	var stack string
+    var name string
+    var birth string
+    var stack string
 
-	err := row.Scan(&name, &birth, &stack)
-	if err == sql.ErrNoRows {
-		return "Nenhuma linha correspondente encontrada.", nil
-	} else if err != nil {
-		return "", err
-	}
+    err := row.Scan(&name, &birth, &stack)
+    if err == sql.ErrNoRows {
+        return "", fmt.Errorf("nenhuma pessoa encontrada com o nickname %s", nickname)
+    } else if err != nil {
+        return "", fmt.Errorf("erro ao buscar pessoa: %v", err)
+    }
 
-	return fmt.Sprintf("Nome: %s, Nascimento: %s, Stack: %s", name, birth, stack), nil
+    return fmt.Sprintf("nome: %s, nascimento: %s, stack: %s", name, birth, stack), nil
 }
 
 func CountRowsInTable() (string, error) {
-	var rowCount int
-	row := Db.QueryRow("SELECT COUNT(*) FROM people")
+    var rowCount int
+    row := Db.QueryRow("SELECT COUNT(*) FROM people")
 
-	err := row.Scan(&rowCount)
-	if err != nil {
-		return "", err
-	}
+    err := row.Scan(&rowCount)
+    if err != nil {
+        return "", fmt.Errorf("erro ao contar linhas na tabela: %v", err)
+    }
 
-	return fmt.Sprintf("Total de linhas na tabela: %d", rowCount), nil
+    return fmt.Sprintf("total de linhas na tabela: %d", rowCount), nil
 }
